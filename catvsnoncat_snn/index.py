0a1f08d8fbd25300489bed6442213af17e2c3d05
scripe2022 <im@jyh.sb>
2022-02-09 01:41:03 -0800

    snn cat

--- catvsnoncat_snn/index.py
+++ catvsnoncat_snn/index.py
@@ -9,11 +9,11 @@ from nnlib.dataset import load_dataset, flatten, normalization
 # init
 np.random.seed(1)
 image_size = 128
-S = [image_size * image_size * 3, 14, 7, 1]
+S = [image_size * image_size * 3, 35, 7, 5, 1]
 # S = [2, 3, 3, 1]
 L = len(S) - 1
 RATE = 0.0075
-STEPS = 3000
+STEPS = 700
 w = [None] * L
 b = [None] * L
 for i in range(len(S) - 1):
@@ -28,12 +28,11 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, label = load_dataset
 )
 train_set_x_flatten = flatten(train_set_x_orig)
 train_set_x = normalization(train_set_x_flatten)
+test_set_x_flatten = flatten(test_set_x_orig)
+test_set_x = normalization(test_set_x_flatten)
 
-# train_set_x = np.loadtxt("datasets/train_set_x.txt")
-# train_set_y = np.loadtxt("datasets/train_set_y.txt")
-# train_set_y = train_set_y.reshape((1, train_set_y.shape[0]))
-
-
+print("RATE: ", RATE)
+print("S: ", S)
 for i in range(STEPS):
     a, J = propagation(train_set_x, train_set_y, w, b)
     dw, db = backpropagation(train_set_x, train_set_y, a, w)
@@ -46,8 +45,26 @@ for i in range(STEPS):
     #     print()
     w = [i - j for i, j in zip(w, [item * RATE for item in dw])]
     b = [i - j for i, j in zip(b, [item * RATE for item in db])]
-    if i % 100 == 0:
-        print("steps: %d, loss: %f" % (i, J))
+    if i % 20 == 0:
+        a_train = propagation(train_set_x, train_set_y, w, b, returnCost=False)
+        train_set_y_hat = np.round(a_train[L])
+        m_train = train_set_y.shape[1]
+        index_train = np.arange(0, m_train)
+        train_accuracy = (
+            len(index_train[train_set_y[0] == train_set_y_hat[0]]) / m_train
+        )
+
+        a_test, J_test = propagation(test_set_x, test_set_y, w, b)
+        test_set_y_hat = np.round(a_test[L])
+        m_test = test_set_y.shape[1]
+        index_test = np.arange(0, m_test)
+        test_accuracy = len(index_test[test_set_y[0] == test_set_y_hat[0]]) / m_test
+
+        print(
+            "steps: {}, loss: {}, train: {:.2%}, test: {:.2%}, test_cost: {}".format(
+                i, J, train_accuracy, test_accuracy, J_test
+            )
+        )
 
 
 save_parameters("parameters.h5", w, b)
