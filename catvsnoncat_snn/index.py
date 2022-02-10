3907d1a10bec3aa51f351caab58d7e2af210b045
scripe2022 <im@jyh.sb>
2022-02-09 18:05:34 -0800

    snn cat

--- catvsnoncat_snn/index.py
+++ catvsnoncat_snn/index.py
@@ -10,19 +10,14 @@ from nnlib.dataset import load_dataset, flatten, normalization
 np.random.seed(1)
 image_size = 128
 S = [image_size * image_size * 3, 35, 7, 5, 1]
-# S = [2, 3, 3, 1]
 L = len(S) - 1
 RATE = 0.0075
-STEPS = 700
+STEPS = 1000
 w = [None] * L
 b = [None] * L
 for i in range(len(S) - 1):
     w[i], b[i] = initialize_parameters(S[i], S[i + 1])
 
-# train set
-# train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, label = load_dataset(
-#     "datasets/train_catvnoncat.h5", "datasets/test_catvnoncat.h5"
-# )
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, label = load_dataset(
     "datasets/train.h5", "datasets/test.h5"
 )
@@ -36,13 +31,6 @@ print("S: ", S)
 for i in range(STEPS):
     a, J = propagation(train_set_x, train_set_y, w, b)
     dw, db = backpropagation(train_set_x, train_set_y, a, w)
-    # force_dw, force_db = force(train_set_x, train_set_y, w, b)
-    # for j in range(len(dw)):
-    #     print(dw[j] - force_dw[j])
-    #     print()
-    #     print(db[j] - force_db[j])
-    #     print()
-    #     print()
     w = [i - j for i, j in zip(w, [item * RATE for item in dw])]
     b = [i - j for i, j in zip(b, [item * RATE for item in db])]
     if i % 20 == 0:
