2d5490ba8a90e4259b94468ce17b10c800c5e47e
scripe2022 <im@jyh.sb>
2022-02-11 22:54:32 -0800

    gradient check

--- snn/hyperparameters.py
+++ snn/hyperparameters.py
@@ -1,7 +1,7 @@
-ALIVE = [1, 1, 0.95, 1]
+ALIVE = [1, 0.95, 1, 1]
 S = [2, 7, 4, 1]
 L = len(S) - 1
-RATE = 0.05
-STEPS = 100000
-LAMBDA = 0.01
+RATE = 0.2
+STEPS = 10000
+LAMBDA = 0.1
 ACT = ["tanh", "tanh", "sigmoid"]
--- snn/index.py
+++ snn/index.py
@@ -2,6 +2,7 @@ import numpy as np
 from utils import *
 from hyperparameters import *
 from plot import plot
+import matplotlib.pyplot as plt
 
 np.random.seed(1)
 
@@ -10,24 +11,32 @@ w, b = initialize_parameters()
 train_set_x, train_set_y = load_datasets(
     "datasets/train_set_x.txt", "datasets/train_set_y.txt"
 )
+test_set_x, test_set_y = load_datasets(
+    "datasets/test_set_x.txt", "datasets/test_set_y.txt"
+)
 
-for i in range(STEPS):
+for i in range(1):
     a, cache = propagation(train_set_x, train_set_y, w, b)
+    a_test, cache_test = propagation(test_set_x, test_set_y, w, b)
     dw, db = backpropagation(train_set_x, train_set_y, a, w, cache["d"])
 
     # gradient check
-    # dw_check, db_check = gradient_check(train_set_x, train_set_y, w, b)
-    # print("{}th step:".format(i))
-    # for j in range(len(dw)):
-    #     print("dW[{}]".format(j))
-    #     print(dw[j] - dw_check[j])
-    #     print("db[{}]".format(j))
-    #     print(db[j] - db_check[j])
-    #     print()
+    dw_check, db_check = gradient_check(train_set_x, train_set_y, w, b)
+    print("{}th step:".format(i))
+    for j in range(len(dw)):
+        print("dW[{}]".format(j))
+        print(dw[j] - dw_check[j])
+        print("db[{}]".format(j))
+        print(db[j] - db_check[j])
+        print()
 
     w = [i - j for i, j in zip(w, [item * RATE for item in dw])]
     b = [i - j for i, j in zip(b, [item * RATE for item in db])]
-    if i % 100 == 0:
-        print("step: {}, loss: {}".format(i, cache["cost"]))
+    if i % 1000 == 0:
+        print(
+            "step: {}, train_loss: {}, test_loss: {}".format(
+                i, cache["cost"], cache_test["cost"]
+            )
+        )
 
 plot(w, b)
