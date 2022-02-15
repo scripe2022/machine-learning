86bd3a32e106efd367d766de8a1eeb089a8cc8f4
scripe2022 <im@jyh.sb>
2022-02-14 17:50:49 -0800

    snn

--- snn/hyperparameters.py
+++ snn/hyperparameters.py
@@ -1,7 +1,8 @@
-ALIVE = [1, 0.5, 1, 1]
+ALIVE = [1, 1, 1, 1]
 S = [2, 7, 4, 1]
 L = len(S) - 1
 RATE = 0.2
 STEPS = 10000
 LAMBDA = 0.1
 ACT = ["tanh", "tanh", "sigmoid"]
+K = 0.9
--- snn/index.py
+++ snn/index.py
@@ -13,26 +13,27 @@ test_set_x, test_set_y = load_datasets(
     "datasets/test_set_x.txt", "datasets/test_set_y.txt"
 )
 
-for i in range(len(w)):
-    print(w[i].shape)
-
-for i in range(10):
+fdw = [np.array(0)] * L
+fdb = [np.array(0)] * L
+for i in range(STEPS + 1):
     a, cache = propagation(train_set_x, train_set_y, w, b)
     a_test, cache_test = propagation(test_set_x, test_set_y, w, b)
     dw, db = backpropagation(train_set_x, train_set_y, a, w, cache["d"])
 
+    fdw = [K * i + (1 - K) * j for i, j in zip(fdw, dw)]
+    fdb = [K * i + (1 - K) * j for i, j in zip(fdb, db)]
     # gradient check
-    dw_check, db_check = gradient_check(train_set_x, train_set_y, w, b)
-    print("{}th step:".format(i))
-    for j in range(len(dw)):
-        print("dW[{}]".format(j))
-        print(dw[j] - dw_check[j])
-        print("db[{}]".format(j))
-        print(db[j] - db_check[j])
-        print()
+    # dw_check, db_check = gradient_check(train_set_x, train_set_y, w, b)
+    # print("{}th step:".format(i))
+    # for j in range(len(dw)):
+    #     print("dW[{}]".format(j))
+    #     print(dw[j] - dw_check[j])
+    #     print("db[{}]".format(j))
+    #     print(db[j] - db_check[j])
+    #     print()
 
-    w = [i - j for i, j in zip(w, [item * RATE for item in dw])]
-    b = [i - j for i, j in zip(b, [item * RATE for item in db])]
+    w = [i - j for i, j in zip(w, [item * RATE for item in fdw])]
+    b = [i - j for i, j in zip(b, [item * RATE for item in fdb])]
     if i % 1000 == 0:
         print(
             "step: {}, train_loss: {}, test_loss: {}".format(
